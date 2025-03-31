7cd7dcf770aedea5166d699a062bbf07ff88ee0e
Sebastian Unterschütz <sebastian@unterschutz.de>
2025-03-31 12:57:09 +0200

    docker-compose.yml add

--- backend/main.go
+++ backend/main.go
@@ -52,8 +52,8 @@ func main() {
 	http.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	// Starte den Server
-	log.Println("Starting server on :8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Println("Starting server on :3000")
+	if err := http.ListenAndServe(":3000", nil); err != nil {
 		log.Fatalf("could not start server: %s\n", err)
 	}
 }
