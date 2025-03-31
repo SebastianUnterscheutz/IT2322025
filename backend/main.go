7087bee09446967f0ebc4eae930b5120a2226477
Sebastian Unterschütz <sebastian@unterschutz.de>
2025-03-31 13:03:44 +0200

    docker-compose.yml add

--- backend/main.go
+++ backend/main.go
@@ -11,42 +11,6 @@ import (
 func main() {
 	staticDir := "./frontend"
 
-	// Handler für die Wurzel-URL
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		files := []string{}
-
-		// Durchlaufe alle Dateien im staticDir
-		err := filepath.Walk(staticDir, func(path string, info os.FileInfo, err error) error {
-			if err != nil {
-				return err
-			}
-			if !info.IsDir() {
-				files = append(files, path)
-			}
-			return nil
-		})
-		if err != nil {
-			http.Error(w, "Could not read files", http.StatusInternalServerError)
-			log.Println("Error walking the path:", err)
-			return
-		}
-
-		// Template um die Dateien anzuzeigen
-		tmpl := `<html><body><h1>Files in /frontend</h1><ul>{{range .}}<li>{{.}}</li>{{end}}</ul></body></html>`
-		t, err := template.New("files").Parse(tmpl)
-		if err != nil {
-			http.Error(w, "Could not create template", http.StatusInternalServerError)
-			log.Println("Template Error:", err)
-			return
-		}
-
-		// Render das Template mit der Liste der Dateien
-		if err := t.Execute(w, files); err != nil {
-			http.Error(w, "Could not execute template", http.StatusInternalServerError)
-			log.Println("Execution Error:", err)
-		}
-	})
-
 	// Erzeuge einen FileServer für das statische Verzeichnis
 	fileServer := http.FileServer(http.Dir(staticDir))
 	http.Handle("/", http.StripPrefix("/", fileServer))
