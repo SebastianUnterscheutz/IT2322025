3eb32ad35da95139b5c6644279454f6a6a4b7b12
Sebastian Unterschütz <sebastian@unterschutz.de>
2025-04-01 08:24:01 +0200

    fix Token add validation

--- backend/routes.go
+++ backend/routes.go
@@ -3,8 +3,11 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
+	"fmt"
 	"log"
+	rand2 "math/rand"
 	"net/http"
+	"regexp"
 	"time"
 )
 
@@ -39,6 +42,39 @@ func createOffer(w http.ResponseWriter, r *http.Request) {
 	// JSON-Daten aus dem Request-Body einlesen
 	if err := json.NewDecoder(r.Body).Decode(&offer); err != nil {
 		http.Error(w, "Invalid input data", http.StatusBadRequest)
+		return
+	}
+
+	offer.Token = fmt.Sprintf("%s-%s-%s-%s", randomString(4), randomString(4), randomString(4), randomString(3))
+
+	// Validate required fields are not empty
+	if offer.Name == "" || offer.FirstName == "" || offer.Email == "" || offer.Class == "" ||
+		offer.PhoneNumber == "" || offer.Token == "" {
+		http.Error(w, "Missing required fields", http.StatusBadRequest)
+		return
+	}
+
+	// Validate email format
+	if !isValidEmail(offer.Email) {
+		http.Error(w, "Invalid email format", http.StatusBadRequest)
+		return
+	}
+
+	// Validate phone number length (e.g., you can adjust as per your system's requirement)
+	if len(offer.PhoneNumber) < 10 || len(offer.PhoneNumber) > 15 {
+		http.Error(w, "Invalid phone number", http.StatusBadRequest)
+		return
+	}
+
+	// Validate 'ValidFrom' and 'ValidUntil' fields
+	if offer.ValidFrom.IsZero() || offer.ValidUntil.IsZero() {
+		http.Error(w, "Both valid_from and valid_until must be provided", http.StatusBadRequest)
+		return
+	}
+
+	// Ensure 'ValidFrom' is before 'ValidUntil'
+	if !offer.ValidFrom.Before(offer.ValidUntil) {
+		http.Error(w, "valid_from must be before valid_until", http.StatusBadRequest)
 		return
 	}
 
@@ -87,6 +123,14 @@ func createOffer(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
+func isValidEmail(email string) bool {
+
+	validEmailRegex := regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+
+	// Validiert eine E-Mail-Adresse anhand von Regex
+	return validEmailRegex.MatchString(email)
+}
+
 func getOffer(w http.ResponseWriter, r *http.Request) {
 	// Überprüfe, ob die Methode GET ist
 	if r.Method != http.MethodGet {
@@ -108,4 +152,14 @@ func getOffer(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Could not encode JSON", http.StatusInternalServerError)
 		return
 	}
+}
+
+func randomString(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	seededRand := rand2.Rand{}
+	result := make([]byte, length)
+	for i := range result {
+		result[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(result)
 }
